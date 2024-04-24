afd1234085a2dc0d4801af0cdebbdba5637c9120
Andrey <aftansin@mail.ru>
2024-04-24 16:30:35 +0300

    async

--- handlers/youtube.py
+++ handlers/youtube.py
@@ -1,14 +1,35 @@
+import os
+
 from aiogram import Router, F, Bot
 from aiogram.types import Message, FSInputFile
 from aiogram.utils.chat_action import ChatActionSender
+from yt_dlp import YoutubeDL
 
 from db.requests import get_user
 from middlewares import RegistrationCheck
-from utils.ytdlp_functions import download_file, delete_file
 
 
 video_router = Router()
 video_router.message.middleware(RegistrationCheck())
+
+
+def download_file(url: str, format_id: str):
+    resolution = format_id[:-1]
+    ydl_opts = {
+        'format': (f'bv*[height<={resolution}][ext=mp4][vcodec~="^((he|a)vc|h26[45])"]+ba[ext=m4a]/b[ext=mp4]'
+                   f' / bv*+ba/b'),
+        'outtmpl': 'Videos/%(id)s.%(ext)s',
+        # 'progress_hooks': [progress],
+        'quiet': True,
+        'no_warnings': True
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        file_info = ydl.extract_info(url, download=True)
+        return file_info
+
+
+async def delete_file(file_name: str):
+    os.remove(f'./Videos/{file_name}')
 
 
 @video_router.message(F.text.startswith('https://www.youtube.com/') |
