9d5331be7e20b35c8e7e558484946877974bd958
Andrey <aftansin@mail.ru>
2024-04-26 13:23:05 +0300

    final corrections

--- db/engine.py
+++ db/engine.py
@@ -1,18 +1,18 @@
 from typing import Union
 
 from sqlalchemy import URL, MetaData
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
 
 
-def get_async_engine(url: Union[URL, str]) -> AsyncEngine:
-    return create_async_engine(url=url, echo=True, pool_pre_ping=True)
+def get_async_engine(url: Union[URL, str]):
+    return create_async_engine(url=url, echo=False, pool_pre_ping=True)
 
 
-def get_session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
+def get_session_maker(engine: AsyncEngine):
     return async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def update_schemas(engine: AsyncEngine, metadata: MetaData) -> None:
+async def update_schemas(engine: AsyncEngine, metadata: MetaData):
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
