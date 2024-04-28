5c4a5e83dce69849482ddd10215dd5922a006e21
Andrey <aftansin@mail.ru>
2024-04-28 13:01:11 +0300

    final corrections

--- handlers/youtube.py
+++ handlers/youtube.py
@@ -15,12 +15,14 @@ video_router = Router()
 video_router.message.middleware(RegistrationCheck())
 
 
-def download_video(url: str, resolution: str):
+async def download_video_async(status_msg, url: str, resolution: str):
     file_name = round(time.time() * 1000)
     ydl_opts = {
         'format': (f'bv*[height<={resolution}][ext=mp4][vcodec~="^((he|a)vc|h26[45])"]+ba[ext=m4a]/b[ext=mp4]'
                    f' / bv*+ba/b'),
         'outtmpl': f'media/{file_name}.%(ext)s',
+        'progress_hooks': [lambda d: status_msg.edit_text(
+            f"{round(d['downloaded_bytes'] / d['total_bytes'] * 100, 2)}%")],
         'quiet': True,
         'no_warnings': True
     }
@@ -29,22 +31,15 @@ def download_video(url: str, resolution: str):
         return [file_name, file_info]
 
 
-async def download_video_async(url: str, resolution: str):
-    loop = asyncio.get_event_loop()
-    info = await loop.run_in_executor(None, download_video, url, resolution)
-    file_name, file_info = info[0], info[1]
-    return [file_name, file_info]
-
-
 @video_router.message(F.text.startswith('https://www.youtube.com/') |
                       F.text.startswith('https://youtu.be/') |
                       F.text.startswith('https://youtube.com/'))
 async def send_video(message: Message, bot: Bot, db_session):
     db_user = await get_user(message.from_user.id, db_session)
-    status_msg = await message.answer('🔣Downloading... Wait.')
+    status_msg = await message.answer('Downloading... Wait.')
     url = message.text
     user_resolution = db_user.quality[:-1]  # user requested video resolution
-    info = await download_video_async(url, user_resolution)
+    info = await download_video_async(status_msg, url, user_resolution)
     file_name = info[0]
     file_info = info[1]
     file_path = file_info['requested_downloads'][0]['filepath']
