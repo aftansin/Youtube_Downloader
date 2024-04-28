01a2c9693062aa9428ab66be0ace8fb7e6e052fe
Andrey <aftansin@mail.ru>
2024-04-28 16:12:11 +0300

    final corrections

--- handlers/youtube.py
+++ handlers/youtube.py
@@ -15,14 +15,12 @@ video_router = Router()
 video_router.message.middleware(RegistrationCheck())
 
 
-async def download_video_async(status_msg, url: str, resolution: str):
+def download_video(url: str, resolution: str):
     file_name = round(time.time() * 1000)
     ydl_opts = {
         'format': (f'bv*[height<={resolution}][ext=mp4][vcodec~="^((he|a)vc|h26[45])"]+ba[ext=m4a]/b[ext=mp4]'
                    f' / bv*+ba/b'),
         'outtmpl': f'media/{file_name}.%(ext)s',
-        'progress_hooks': [lambda d: status_msg.edit_text(
-            f"{round(d['downloaded_bytes'] / d['total_bytes'] * 100, 2)}%")],
         'quiet': True,
         'no_warnings': True
     }
@@ -31,23 +29,30 @@ async def download_video_async(status_msg, url: str, resolution: str):
         return [file_name, file_info]
 
 
+async def download_video_async(url: str, resolution: str):
+    loop = asyncio.get_event_loop()
+    info = await loop.run_in_executor(None, download_video, url, resolution)
+    file_name, file_info = info[0], info[1]
+    return [file_name, file_info]
+
+
 @video_router.message(F.text.startswith('https://www.youtube.com/') |
                       F.text.startswith('https://youtu.be/') |
                       F.text.startswith('https://youtube.com/'))
 async def send_video(message: Message, bot: Bot, db_session):
     db_user = await get_user(message.from_user.id, db_session)
-    status_msg = await message.answer('Downloading... Wait.')
+    status_msg = await message.answer('⬇️ Downloading... Wait.')
     url = message.text
     user_resolution = db_user.quality[:-1]  # user requested video resolution
-    info = await download_video_async(status_msg, url, user_resolution)
+    info = await download_video_async(url, user_resolution)
     file_name = info[0]
     file_info = info[1]
     file_path = file_info['requested_downloads'][0]['filepath']
     try:
-        await status_msg.edit_text('Sending file to Telegram...')
+        await status_msg.edit_text('⬆️ Sending file to Telegram...')
         try:
             async with ChatActionSender.upload_video(message.chat.id, bot):
-                await message.reply_video(
+                await message.answer_video(
                     video=FSInputFile(file_path),
                     duration=file_info.get('duration'),
                     width=file_info.get('width'),
@@ -62,6 +67,7 @@ async def send_video(message: Message, bot: Bot, db_session):
                 os.remove(file['filepath'])
         finally:
             await status_msg.delete()
+            await message.delete()
     except Exception as e:
         if isinstance(e, yt_dlp.utils.DownloadError):
             await message.answer(f'Invalid URL\n{e}')
