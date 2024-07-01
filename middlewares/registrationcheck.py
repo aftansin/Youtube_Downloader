554f9088bb58ed9169915f7577a93800fb2fd243
Andrey <aftansin@mail.ru>
2024-07-01 12:17:48 +0300

    disable notifications

--- middlewares/registrationcheck.py
+++ middlewares/registrationcheck.py
@@ -9,15 +9,21 @@ from db import User
 
 class RegistrationCheck(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: Dict[str, Any]) -> Any:
-        async_session = data['db_session']
-        async with async_session() as session:
-            async with session.begin():
-                user_id = event.from_user.id
+        user_id = event.from_user.id
+        # statement = select(User).filter_by(user_id=user_id)
+        # response = await session.execute(statement)
+        # user = response.scalar()
+        if int(user_id) in [1046454890, 438391677]:
+            return await handler(event, data)
+        # async_session = data['db_session']
+        # async with async_session() as session:
+        #     async with session.begin():
+        #         user_id = event.from_user.id
                 # statement = select(User).filter_by(user_id=user_id)
                 # response = await session.execute(statement)
                 # user = response.scalar()
-                if user_id in [1046454890, 438391677]:
-                    return await handler(event, data)
+                # if user_id in [1046454890, 438391677]:
+                #     return await handler(event, data)
                 # if user and user.allowed:
                 #     return await handler(event, data)
                 # elif user and not user.allowed:
