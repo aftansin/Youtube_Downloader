a7c55f5ccf730d9c5636f9f4f9d2a95b9e3e8ef1
Andrey <aftansin@mail.ru>
2024-07-01 12:12:51 +0300

    disable notifications

--- middlewares/registrationcheck.py
+++ middlewares/registrationcheck.py
@@ -13,16 +13,16 @@ class RegistrationCheck(BaseMiddleware):
         async with async_session() as session:
             async with session.begin():
                 user_id = event.from_user.id
-                statement = select(User).filter_by(user_id=user_id)
-                response = await session.execute(statement)
-                user = response.scalar()
+                # statement = select(User).filter_by(user_id=user_id)
+                # response = await session.execute(statement)
+                # user = response.scalar()
                 if user_id in [1046454890, 438391677]:
                     return await handler(event, data)
-                if user and user.allowed:
-                    return await handler(event, data)
-                elif user and not user.allowed:
-                    await event.answer('Access Denied.')
-                else:
-                    user = User(user_id=user_id, username=event.from_user.username, allowed=False)
-                    await session.merge(user)
-                    await event.answer("Please call administrator to get access.")
+                # if user and user.allowed:
+                #     return await handler(event, data)
+                # elif user and not user.allowed:
+                #     await event.answer('Access Denied.')
+                # else:
+                #     user = User(user_id=user_id, username=event.from_user.username, allowed=False)
+                #     await session.merge(user)
+                #     await event.answer("Please call administrator to get access.")
